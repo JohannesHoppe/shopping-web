2d794e06169c4d5103ba3d24c4192f2aade6ad36
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-03-20 17:38:54 +0100

    deleting dist folder

--- dist/app.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {ShoppingWebApp} from './app/shopping-web';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-
-bootstrap(ShoppingWebApp, [
-  ROUTER_PROVIDERS
-]);
